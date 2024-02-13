7c499c586e30dbdd9610aedfb031fb90bb7b04ca
Argel Hernandez Amaya <argel6767@gmail.com>
2024-02-12 19:31:28 -0500

    gave submitIcon an event and started fetch function

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const API_KEY = "YOUR API KEY HERE"
+
+const submitIcon = document.querySelector("#submit-icon")
+
+function getImages() {
+    try {
+        fetch ('https://api.openai.com/v1/images/generations')
+    }
+    catch (error) {
+        console.error(error)
+    }
+    
+}
+
+submitIcon.addEventListener('click', getImages) 
+
+
 
