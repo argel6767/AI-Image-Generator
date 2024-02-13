061d2381b16c0848cfc92fb1f582da734f434414
Argel Hernandez Amaya <argel6767@gmail.com>
2024-02-12 19:32:59 -0500

    more work on getImages()

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const API_KEY = "YOUR API KEY HERE"
 const submitIcon = document.querySelector("#submit-icon")
 
 function getImages() {
+    const options = {
+        
+    }
     try {
-        fetch ('https://api.openai.com/v1/images/generations')
+        fetch ('https://api.openai.com/v1/images/generations', options)
     }
     catch (error) {
         console.error(error)
